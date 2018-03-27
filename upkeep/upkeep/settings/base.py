7cad6026e869b5ea86d9cab7643f9ebb7a05bfd4
developertqw2017 <2144799613@qq.com>
2018-03-27 22:59:36 +0800

    migrate mysql

--- upkeep/upkeep/settings/base.py
+++ upkeep/upkeep/settings/base.py
@@ -91,8 +91,12 @@ WSGI_APPLICATION = 'upkeep.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'upkeep',
+        'USER':'root',
+        'PASSWORD':'tqw503417',
+        'HOST':'127.0.0.1',
+        'PORT':'3306',
     }
 }
 
